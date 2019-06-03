d9483ccd6d9e3ed0665077fb6595bee3b225f2f8
João Paulo <paulospc21@gmail.com>
2019-06-03 17:05:18 -0300

    Acessors

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function getResumeTitleAttribute(){
+        if (isset($this->attributes['title'][3])){
+        return mb_substr($this->attributes['title'], 0, 3) . '...';
+        }
+
+        return $this->attributes['title'];
+    }
 }
