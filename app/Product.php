627101437ea3777de8da178cf5cc8ff841f3999b
João Paulo <paulospc21@gmail.com>
2019-06-05 10:48:58 -0300

    Mutators

--- app/Product.php
+++ app/Product.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['title', 'description'];
+
     public function getResumeTitleAttribute(){
         if (isset($this->attributes['title'][3])){
         return mb_substr($this->attributes['title'], 0, 3) . '...';
         }
 
         return $this->attributes['title'];
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = strtoupper($value);
     }
 }
