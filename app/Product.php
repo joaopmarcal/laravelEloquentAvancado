cc1d6934fc0d0cc81af2400722e228787c0149af
João Paulo <paulospc21@gmail.com>
2019-06-05 11:04:12 -0300

    Serialização

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['title', 'description'];
+    protected $appends = ['resume_title'];
+    protected $hidden = ['title'];
 
     public function getResumeTitleAttribute(){
         if (isset($this->attributes['title'][3])){
